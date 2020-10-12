505d9ea7b97d3c7da9f27bfa1b4937a8f826f734
cornel-kim <cornelkemboi@yahoo.com>
2020-10-12 06:37:02 +0300

    First commit

--- app/src/main/java/com/modcom/pledgeapp/MainActivity.kt
+++ app/src/main/java/com/modcom/pledgeapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.modcom.pledgeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Our first class 2020")
     }
 }
